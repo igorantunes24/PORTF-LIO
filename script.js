cb50e7e105a962088be09d2a2b3a35242a552837
igorantunes24 <igor.antunes24@escola.pr.gov.br>
2025-04-10 07:58:49 -0300

    hckkklljl

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',function(){
+const aumentaFonteBotao=document.getElementByld('aumentar-fonte');
+const diminuiFonteBotao=document.getElementByld('diminuir-fonte');
+
+let tamanhoAtualFonte=1;
+
+aumentaFonteBotao.addEventListener('click',function(){
+tamanhoAtualFonte+=0.1;
+document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+
+})
+
+diminuiFonteBotao.addEventListener('click',function(){
+    tamanhoAtualFonte-=0.1;
+    document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+    
+    })
