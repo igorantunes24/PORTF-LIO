ae7ec403b0dc148180c8ecb129888c24150a7b8e
igorantunes24 <igor.antunes24@escola.pr.gov.br>
2025-04-10 08:17:53 -0300

    nmn,k

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-document.addEventListener('DOMContentLoaded',function(){
-const aumentaFonteBotao=document.getElementByld('aumentar-fonte');
-const diminuiFonteBotao=document.getElementByld('diminuir-fonte');
+document.addEventListener('DOMContentLoaded', function () {
 
-let tamanhoAtualFonte=1;
-
-aumentaFonteBotao.addEventListener('click',function(){
-tamanhoAtualFonte+=0.1;
-document.body.style.fontSize=`${tamanhoAtualFonte}rem`
-
-})
-
-diminuiFonteBotao.addEventListener('click',function(){
-    tamanhoAtualFonte-=0.1;
-    document.body.style.fontSize=`${tamanhoAtualFonte}rem`
-    
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+ 
+    let tamanhoAtualFonte = 1;
+ 
+    aumentaFonteBotao.addEventListener('click', function () {
+         tamanhoAtualFonte += 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+ 
     })
-})
+ 
+    diminuiFonteBotao.addEventListener('click', function () {
+         tamanhoAtualFonte -= 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+ 
+    })
+ 
+ })
